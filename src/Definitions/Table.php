c592722ffafb675128bf1beac3a8f5e802dabff7
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-01-15 13:12:02 +0000

    Triun Table Column type

--- src/Definitions/Table.php
+++ src/Definitions/Table.php
@@ -2,9 +2,30 @@
 
 namespace Triun\ModelBase\Definitions;
 
+use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\DBAL\Schema\Table as DoctrineTable;
 
 class Table extends DoctrineTable
 {
+    /**
+     * @return Column[]
+     */
+    public function getColumns()
+    {
+        parent::getColumns();
+    }
 
+    /**
+     * Returns the Column with the given name.
+     *
+     * @param string $columnName The column name.
+     *
+     * @return Column
+     *
+     * @throws SchemaException If the column does not exist.
+     */
+    public function getColumn($columnName)
+    {
+        parent::getColumn($columnName);
+    }
 }
