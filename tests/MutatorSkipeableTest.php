779bc4570cb464cd8ae24f695bdb61e24807052e
Gonzalo Moreno <gonzalomf@healthwave.ie>
2017-01-16 01:25:21 +0000

    Add Orchestra Testbench and update test case

--- tests/MutatorSkipeableTest.php
+++ tests/MutatorSkipeableTest.php
@@ -1,8 +1,92 @@
 <?php
 
-
+/**
+ * Class MutatorSkipeableTest
+ * @see \Triun\ModelBase\MutatorSkipeable
+ */
 class MutatorSkipeableTest extends TestCase
 {
+    /**
+     * Original string given.
+     *
+     * @var string
+     */
+    protected $original = 'Some title';
+
+    /**
+     * Expected mutation when set.
+     *
+     * @var string
+     */
+    protected $setterMutator = 'Native title';
+
+    /**
+     * Expected mutation when get.
+     *
+     * @var string
+     */
+    protected $getterMutator = 'Formatted title';
+
+    /**
+     * @test
+     */
+    function it_has_getter_and_setter_mutators()
+    {
+        $model = $this->getPost();
+
+        $model->title = $this->original;
+
+        $native = array_get($model->getAttributes(), 'title');
+
+        // Native (setter mutation)
+        $this->assertEquals(
+            $this->setterMutator,
+            $native
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->getAttribute('title')
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->title
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_sets_an_attribute_value_without_mutator()
+    {
+        $model = $this->getPost();
+
+        $model->run_setAttributeWithoutMutator('title', $this->original);
+
+        $native = array_get($model->getAttributes(), 'title');
+
+        // Native (setter mutation)
+        $this->assertEquals(
+            $this->original,
+            $native
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->getAttribute('title')
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->title
+        );
+    }
+
     /**
      * @test
      */
@@ -10,26 +94,213 @@ class MutatorSkipeableTest extends TestCase
     {
         $model = $this->getPost();
 
-        $model->title = 'Some title';
+        $model->title = $this->original;
 
+        $native = array_get($model->getAttributes(), 'title');
+
+        // Native (setter mutation)
         $this->assertEquals(
-            $model->getAttribute('title'),
-            'Title: Some title'
+            $this->setterMutator,
+            $native
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->getAttribute('title')
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->title
         );
 
         $this->assertEquals(
-            $model->title,
-            'Title: Some title'
+            $native,
+            $model->getAttributeValueWithoutMutator('title')
         );
 
         $this->assertEquals(
-            array_get($model->getAttributes(), 'title'),
-            'Some title'
+            $this->setterMutator,
+            $model->getAttributeValueWithoutMutator('title')
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_sets_and_gets_an_attribute_value_without_mutator()
+    {
+        $model = $this->getPost();
+
+        $model->run_setAttributeWithoutMutator('title', $this->original);
+
+        $native = array_get($model->getAttributes(), 'title');
+
+        // Native (setter mutation)
+        $this->assertEquals(
+            $this->original,
+            $native
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->getAttribute('title')
+        );
+
+        // Getter mutation
+        $this->assertEquals(
+            $this->getterMutator,
+            $model->title
         );
 
         $this->assertEquals(
-            $model->getAttributeValueWithoutMutator('title'),
-            'Some title'
+            $this->original,
+            $model->getAttributeValueWithoutMutator('title')
         );
+    }
+
+    /**
+     * @test
+     */
+    function it_does_casts()
+    {
+        $model = $this->getPost();
+
+        $metadata = [
+            'foo' => 'bar',
+        ];
+
+        $json = json_encode($metadata);
+
+        $model->run_setAttributeWithoutMutator('metadata', $metadata);
+
+        $native = array_get($model->getAttributes(), 'metadata');
+
+        // Native (setter mutation)
+        $this->assertEquals(
+            $json,
+            $native
+        );
+
+        $this->assertEquals(
+            $metadata,
+            $model->getAttributeValueWithoutMutator('metadata')
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_does_dates()
+    {
+        $model = $this->getPost();
+
+        $carbon = \Carbon\Carbon::now();
+
+        $datetime = $carbon->toDateTimeString();
+
+        $model->run_setAttributeWithoutMutator('updated_at', $carbon);
+
+        $native = array_get($model->getAttributes(), 'updated_at');
+
+        // Native (setter mutation)
+        $this->assertEquals(
+            $datetime,
+            $native
+        );
+
+        $this->assertInstanceOf(
+            \Carbon\Carbon::class,
+            $model->getAttributeValueWithoutMutator('updated_at')
+        );
+
+        $this->assertEquals(
+            $carbon,
+            $model->getAttributeValueWithoutMutator('updated_at')
+        );
+    }
+
+    /**
+     * Retrieve a example table.
+     *
+     * @return Post
+     */
+    protected function getPost()
+    {
+        $post = new Post();
+
+        $post->title = 'Some title';
+        $post->metadata = [
+            'foo' => 'bar',
+        ];
+        $post->save();
+
+        return $post;
+    }
+}
+
+/**
+ * Class Post
+ *
+ * @property string $title
+ * @property array|object $metadata
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Post extends \Illuminate\Database\Eloquent\Model
+{
+    use \Triun\ModelBase\MutatorSkipeable;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'metadata' => 'array',
+    ];
+
+    /**
+     * Set a given attribute on the model, without using the mutator.
+     * Add phone type functionality.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return $this
+     */
+    public function run_setAttributeWithoutMutator($key, $value)
+    {
+        return $this->setAttributeWithoutMutator($key, $value);
+    }
+
+    /**
+     * Get a plain attribute (not a relationship), without using the mutator.
+     * Add phone type functionality.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function run_getAttributeValueWithoutMutator($key)
+    {
+        return $this->getAttributeValueWithoutMutator($key);
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitleAttribute()
+    {
+        return str_replace(['Native', 'Some'], 'Formatted', $this->attributes['title']);
+    }
+
+    /**
+     * @param $value
+     */
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = str_replace('Some', 'Native', $value);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,53 +1,195 @@
 <?php
 
-use Illuminate\Database\Capsule\Manager as DB;
-
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends Orchestra\Testbench\TestCase
 {
+    /**
+     * Default connection name for the tests.
+     */
+    const TEST_CONNECTION = 'testing';
+
+    /**
+     * If True, the default migration will be applied.
+     */
+    const DEFAULT_MIGRATION = true;
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     *
+     */
     public function setUp()
     {
-        $this->setUpDatabase();
-        $this->migrateTables();
+        parent::setUp();
+
+        // It is supposed to be added by Orchestra, but it's not, so we add it manually.
+        $provider = new \Triun\ModelBase\ModelBaseServiceProvider($this->app);
+        $provider->boot();
+//        dd($this->app['config']['model-base']);
+
+        if (static::DEFAULT_MIGRATION) {
+            $this->migrateUp();
+        }
     }
 
-    protected function setUpDatabase()
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+    public function tearDown()
     {
-        $database = new DB;
+        if (static::DEFAULT_MIGRATION) {
+            $this->migrateDown();
+        }
 
-        $database->addConnection(['driver' => 'sqlite', 'database' => ':memory:']);
-        $database->bootEloquent();
-        $database->setAsGlobal();
+        parent::tearDown();
     }
 
-    protected function migrateTables()
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
     {
-        DB::schema()->create('posts', function (\Illuminate\Database\Schema\Blueprint $table) {
+        return [\Triun\ModelBase\ModelBaseServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', static::TEST_CONNECTION);
+        $app['config']->set('database.connections.'.static::TEST_CONNECTION, $this->getDefaultDatabaseConfig());
+    }
+
+    /**
+     * Get application timezone.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return string|null
+     */
+    protected function getApplicationTimezone($app)
+    {
+        return 'Asia/Kuala_Lumpur';
+    }
+
+    /**
+     * Retrieve the default database configuration.
+     *
+     * @return array
+     * @throws Exception
+     */
+    private function getDefaultDatabaseConfig()
+    {
+        switch (env('DB_TEST_DRIVER', null)) {
+            case 'sqlite':
+                return [
+                    'driver'   => 'sqlite',
+                    'database' => env('DB_TEST_DATABASE', ':memory:'), //database_path('database.sqlite')),
+                    'prefix'   => '',
+                ];
+                break;
+            case 'mysql':
+                return [
+                    'driver' => 'mysql',
+                    'host' => env('DB_TEST_HOST', '127.0.0.1'),
+                    'port' => env('DB_TEST_PORT', '3306'),
+                    'database' => env('DB_TEST_DATABASE', 'testing'),
+                    'username' => env('DB_TEST_USERNAME', 'testing'),
+                    'password' => env('DB_TEST_PASSWORD', ''),
+                    'charset' => 'utf8',
+                    'collation' => 'utf8_unicode_ci',
+                    'prefix' => '',
+                    'strict' => true,
+                    'engine' => null,
+                ];
+                break;
+            case 'pgsql':
+                return [
+                    'driver' => 'pgsql',
+                    'host' => env('DB_TEST_HOST', '127.0.0.1'),
+                    'port' => env('DB_TEST_PORT', '5432'),
+                    'database' => env('DB_TEST_DATABASE', 'testing'),
+                    'username' => env('DB_TEST_USERNAME', 'testing'),
+                    'password' => env('DB_TEST_PASSWORD', ''),
+                    'charset' => 'utf8',
+                    'prefix' => '',
+                    'schema' => 'public',
+                    'sslmode' => 'prefer',
+                ];
+                break;
+            case null:
+                throw new Exception('DB_TEST_DRIVER not defined.');
+            default:
+                throw new Exception('DB_TEST_DRIVER '.env('DB_TEST_DRIVER').' does\'t exists.');
+        }
+    }
+
+    /**
+     * Migrate up
+     */
+    protected function migrateUp()
+    {
+        Schema::create('users', function (\Illuminate\Database\Schema\Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('password');
+            $table->timestamps();
+        });
+
+        Schema::create('posts', function (\Illuminate\Database\Schema\Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->text('metadata');
             $table->timestamps();
         });
     }
 
-    protected function getPost()
+    /**
+     * Migrate down
+     */
+    protected function migrateDown()
     {
-        $post = new Post();
-
-        $post->title = 'Some title';
-        $post->save();
-
-        return $post;
+        Schema::drop('users');
+        Schema::drop('posts');
     }
-}
-
-class Post extends \Illuminate\Database\Eloquent\Model
-{
-    use \Triun\ModelBase\MutatorSkipeable;
 
     /**
-     * @return string
+     * Retrieve a connection instance.
+     *
+     * @param string $connection
+     *
+     * @return \Illuminate\Database\Connection
      */
-    public function getTitleAttribute()
+    protected function getConnection($connection = 'testing')
     {
-        return 'Title: '.$this->attributes['title'];
+        return DB::connection($connection);
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @return \Triun\ModelBase\ModelBaseConfig
+     */
+    protected function getConfig($connection = 'testing')
+    {
+        return new \Triun\ModelBase\ModelBaseConfig($this->getConnection($connection));
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @return \Triun\ModelBase\Utils\SchemaUtil
+     */
+    protected function getSchemaUtil($connection = 'testing')
+    {
+        return new \Triun\ModelBase\Utils\SchemaUtil($this->getConnection($connection), $this->getConfig($connection));
     }
 }
